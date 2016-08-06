650f1dffad432f77364bd0e6db7698db1a0616d6
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2016-08-06 03:08:02 -0300

    Finalizando programa

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,32 @@
 // arquivo de script próprio
 function limparBanco() {
-    $.ajax({
-        url: 'http://' + window.location.host + '/limparBanco',
-        type: "GET",
-        //data: 'your form data',
-        success: function (response) {
-            alert('Banco esvaziado com sucesso!');
-        }
-    });
+    if (confirm('Tem certeza que quer apagar todos os dados?')) {
+        $.ajax({
+            url: 'http://' + window.location.host + '/limparBanco',
+            type: "GET",
+            //data: 'your form data',
+            success: function (response) {
+                alert('Banco esvaziado com sucesso!');
+            }
+        });
+    }
 }
 
 // salva o nome de um produto no banco
 function salvarNome() {
     const tag = document.getElementById('selectTag').value
     const nome = document.getElementById('txtNomeProduto').value
-    
+
     if (!nome || nome === '') {
         alert('Você precisa selecionar um nome para o produto!')
         return
     }
-    
+
     const produto = {
         tagProduto: tag,
         nomeProduto: nome
     }
-    
+
     $.ajax({
         url: 'http://' + window.location.host + '/salvarNome',
         type: "POST",
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const databaseCredenciais = 'admin:mongo.oobj@';
 const databaseUrl = databaseCredenciais + '127.0.0.1:27017/controle-de-estoque';
 const collectionProdutos = 'produtos'
 const collectionDurabilidade = 'durabilidades'
+// fazer a contagem de quantos produtos devem ser contados para gerar a média
+const collectionConfiguracoes = 'config'
 
 // apenas para remoção de problemas de CORS
 const allowCrossDomain = function (req, res, next) {
@@ -41,37 +43,94 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+function formatarData(data) {
+    var meses = [
+        "Janeiro", "Fevereiro", "Março",
+        "Abril", "Maio", "Junho", "Julho",
+        "Agosto", "Setembro", "Outubro",
+        "Novembro", "Dezembro"
+    ]
+
+    var dia = data.getDate()
+    var mesIndice = data.getMonth()
+    var ano = data.getFullYear()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    var segundos = data.getSeconds()
+
+    return dia + ' de ' + meses[mesIndice] + ' de ' + ano + ' ' + hora + 'h ' + minutos + 'm ' + segundos + 's'
+}
+
+function milissegundosParaTempoLegivel(milissegundos) {
+
+    function addZero(numero) {
+        return (numero < 10 ? '0' : '') + numero;
+    }
+
+    var mili = milissegundos % 1000;
+    milissegundos = (milissegundos - mili) / 1000;
+    var segundos = milissegundos % 60;
+    milissegundos = (milissegundos - segundos) / 60;
+    var minutos = milissegundos % 60;
+    var horas = (milissegundos - minutos) / 60;
+
+    return addZero(horas) + 'h ' + addZero(minutos) + 'm ' + addZero(segundos) + 's';
+}
+
 /**
  * Roda para listagem dos produtos cadastrados no sistema
  */
-app.get('/listagem', (req, res) => {
-    database.collection(collectionProdutos).find().toArray((err, result) => {
-        var content = {
-            produtos: []
-        }
-
-        if (result) content.produtos = result
-        res.render('lista', content)
-    })
-})
-
-/**
- * Rota que cadastra uma nova TAG
- */
-app.get('/cadastro', (req, res) => {
+app.get('/estoque', (req, res) => {
     database.collection(collectionProdutos).group(
-        ['tagProduto', 'nomeProduto'], // agrupando por tagProduto e nomeProduto
-        {}, // nenhum filtro visto que quero todos os produtos do banco
-        { "quantidadeProduto": 0 }, // quero esse campo para todos os resultados retornados
-        "function (obj, prev) { prev.quantidadeProduto++; }", // incrementando o campo falso
+        { 'nomeProduto': true }, // agrupando por nomeProduto
+        { 'dataSaida': null }, // apenas os produtos que ainda não foram removidos
+        { 'quantidadeProduto': 0, 'durabilidadeTotal': 0 }, // quero esse campo para todos os resultados retornados
+        "function (obj, prev) { prev.quantidadeProduto++; prev.durabilidadeTotal += obj.durabilidade; prev.dataEntradaProd = obj.dataEntradaTimestamp; }", // quantidade e durabilidade
         function (err, results) {
+
+            if (results) {
+                results.forEach(function (resultado) {
+                    var dataAtual = new Date();
+                    var diferenca = -1
+
+                    if (dataAtual.getTime() - resultado.dataEntradaProd.getTime() < resultado.durabilidadeTotal) {
+                        diferenca = dataAtual.getTime() - resultado.dataEntradaProd.getTime()
+                    }
+
+                    resultado.durabilidadeTotal =
+                        milissegundosParaTempoLegivel(diferenca > 0 ? resultado.durabilidadeTotal - diferenca : 0)
+                });
+            }
+
             var content = {
                 produtos: []
             }
 
             if (results && results.length > 0) content.produtos = results
-            res.render('cadastro', content)
+            res.render('estoque', content)
         })
+})
+
+/**
+ * Rota que cadastra uma nova TAG
+ */
+app.get('/auditoria', (req, res) => {
+    database.collection(collectionProdutos).find({}).sort({ 'dataEntradaTimestamp': 1 }).toArray((err, results) => {
+        var content = {
+            produtos: [],
+            tags: []
+        }
+
+        if (results && results.length > 0) {
+            content.produtos = results
+            results.forEach(function (resultado) {
+                if (content.tags.indexOf(resultado.tagProduto) == -1) {
+                    content.tags.push(resultado.tagProduto)
+                }
+            });
+        }
+        res.render('auditoria', content)
+    })
 })
 
 /**
@@ -83,7 +142,17 @@ app.get('/vencimentos', (req, res) => {
             durabilidades: []
         }
 
-        if (result) content.durabilidades = result
+        if (result && result.length > 0) {
+
+            result.forEach(function (durabilidade) {
+                content.durabilidades.push({
+                    nomeProduto: durabilidade.nomeProduto,
+                    total: milissegundosParaTempoLegivel(durabilidade.total),
+                    unidade: milissegundosParaTempoLegivel(durabilidade.unidade)
+                })
+            });
+        }
+
         res.render('vencimentos', content)
     })
 })
@@ -107,10 +176,27 @@ function persistirDurabilidade(durabilidade, atualizar) {
             resultDurabilidade.total = durabilidade.total
             resultDurabilidade.unidade = durabilidade.unidade
 
-            database.collection(collectionDurabilidade).save(resultDurabilidade, (err, result) => {
-                if (err) return false
-                else true
-            })
+            // atualizando a durabilidade
+            database.collection(collectionProdutos).update(
+                { tagProduto: produto.tagProduto, dataEntradaTimestamp: produto.dataEntradaTimestamp }, // procurarei pelo produto específico
+                {
+                    "$set": {
+                        "dataSaida": produto.dataSaida,
+                        "dataSaidaTimestamp": produto.dataSaidaTimestamp,
+                        "durabilidade": produto.durabilidade
+                    }
+                },
+                { "multi": false }, // como quero atualizar apenas um documento
+                (err, result, status) => {
+                    if (err) {
+                        console.log(err)
+                        res.json({ success: false })
+                        return
+                    }
+
+                    console.log('Saída do produto ' + produto.tagProduto + ' realizada com sucesso.')
+                    res.json({ success: true })
+                })
 
         }
     })
@@ -173,27 +259,66 @@ function persistirProduto(produto) {
 app.get('/entrada', (req, res) => {
 
     const tagProduto = req.query.tagProduto
-    const dataAtual = new Date();
-    const dataDeEntrada = dataAtual.getDay() + '/' +
-        Number(dataAtual.getMonth() + 1) + '/' +
-        dataAtual.getFullYear() + ' ' +
-        dataAtual.getHours() + ':' +
-        dataAtual.getMinutes() + ':' +
-        dataAtual.getSeconds()
+    const dataEntradaTimestamp = new Date();
 
-    database.collection(collectionProdutos).findOne({ 'tagProduto': tagProduto }, (err, produto) => {
+    console.log('Procurando produtos com tag: ' + tagProduto)
+
+    database.collection(collectionProdutos).find({ 'tagProduto': tagProduto }).toArray((err, produtos) => {
         // se conseguir encontrar o documento no banco aumenta sua quantidade e salva
+        var isProdutoPersistido = false
         if (!err) {
-            // criando os dados do novo produto
+            // criando os dados do novo produto se o mesmo não existir ainda
+
             var novoProduto = {}
-            novoProduto.nomeProduto = produto ? produto.nomeProduto : ''
             novoProduto.tagProduto = tagProduto
-            novoProduto.dataEntrada = dataDeEntrada
+            novoProduto.dataEntrada = formatarData(dataEntradaTimestamp)
+            novoProduto.dataEntradaTimestamp = dataEntradaTimestamp
             novoProduto.dataSaida = null
-            novoProduto.durabilidade = produto ? recalcularDurabilidadeEntrada(produto) : recalcularDurabilidadeEntrada(novoProduto)
-            var retorno = persistirProduto(novoProduto)
-            res.json({ success: retorno })
+            novoProduto.dataSaidaTimestamp = null
+
+            if (produtos && produtos.length > 0) {
+
+                var produto = {
+                    dataSaida: null
+                }
+
+                var existeProdutoSemSaida = false
+
+                produtos.forEach(function (prod) {
+                    novoProduto.nomeProduto = prod.nomeProduto
+                    if (prod.dataSaida == null) {
+                        produto = prod
+                        existeProdutoSemSaida = true
+                    }
+                })
+
+                database.collection(collectionDurabilidade).findOne({ 'nomeProduto': novoProduto.nomeProduto }, (err, durabilidade) => {
+                    novoProduto.durabilidade = 0
+                    if (err) console.log(err)
+                    if (durabilidade) {
+                        novoProduto.durabilidade = durabilidade.unidade
+                    }
+                    console.log('Durabilidade calculada: ' + novoProduto.durabilidade)
+
+                    if (!existeProdutoSemSaida || produto.dataSaida != null) {
+                        isProdutoPersistido = persistirProduto(novoProduto)
+                    }
+
+                    res.json({ success: isProdutoPersistido })
+                })
+
+            } else {
+
+                novoProduto.nomeProduto = ''
+                novoProduto.durabilidade = 0
+                isProdutoPersistido = persistirProduto(novoProduto)
+                res.json({ success: isProdutoPersistido })
+
+            }
+        } else {
+            console.log('Erro ao criar entrada: ' + err)
         }
+
     })
 })
 
@@ -201,17 +326,91 @@ app.get('/entrada', (req, res) => {
  * Rota que registra a saída de um produto
  */
 app.get('/saida', (req, res) => {
-    database.collection(collectionProdutos).findOne({ tagProduto: req.query.tagProduto }, (err, produto) => {
-        // se conseguir encontrar o documento no banco diminui sua quantidade e salva
-        if (!err) {
-            produto.qtdProduto = Number(produto.qtdProduto) - 1
-            database.collection(collectionProdutos).save(produto, (err, result) => {
-                if (err) return console.log(err)
-                res.redirect('/cadastro')
-            })
+
+    const tagProduto = req.query.tagProduto
+    const dataSaidaTimestamp = new Date();
+
+    database.collection(collectionProdutos).findOne({ 'tagProduto': tagProduto, 'dataSaida': null }, (err, produto) => {
+        // se conseguir encontrar o documento no banco realiza a saída do mesmo e pronto
+        if (err) return console.log('Erro ao procurar arquivo de saída ' + err)
+        else if (produto) {
+            produto.dataSaida = formatarData(dataSaidaTimestamp)
+            produto.dataSaidaTimestamp = dataSaidaTimestamp
+            produto.durabilidade = Math.abs(produto.dataSaidaTimestamp - produto.dataEntradaTimestamp)
+            atualizarProduto(produto)
+
+            res.json({ success: true })
         }
     })
 })
+
+function atualizarProduto(produto) {
+    database.collection(collectionProdutos).update(
+        { 'tagProduto': produto.tagProduto, 'dataEntradaTimestamp': produto.dataEntradaTimestamp }, // procurarei pelo produto específico
+        {
+            "$set": {
+                "dataSaida": produto.dataSaida,
+                "dataSaidaTimestamp": produto.dataSaidaTimestamp,
+                "durabilidade": produto.durabilidade
+            }
+        },
+        { 'multi': false }, // como quero atualizar apenas um produto
+        (err, result, status) => {
+            if (err || result <= 0) {
+                console.log(err)
+                return
+            }
+
+            recalcularDurabilidadeSaida(produto)
+            console.log('Saída do produto ' + produto.tagProduto + ' realizada com sucesso com nova durabiliade de ' + milissegundosParaTempoLegivel(produto.durabilidade))
+        })
+}
+
+function recalcularDurabilidadeSaida(produto) {
+
+    var produtosLength = 1
+
+    database.collection(collectionProdutos).find({ 'nomeProduto': produto.nomeProduto }).toArray(function (err, produtos) {
+        if (!err) produtosLength = produtos.length
+
+        var durabilidade = {
+            nomeProduto: produto.nomeProduto,
+            total: produto.durabilidade
+        }
+
+        database.collection(collectionDurabilidade).findOne({ 'nomeProduto': produto.nomeProduto }, (err, durabilidadeBanco) => {
+            if (durabilidadeBanco) {
+                durabilidade = durabilidadeBanco
+                durabilidade.total += produto.durabilidade
+            }
+
+            durabilidade.unidade = durabilidade.total / produtosLength
+            atualizarDurabilidade(durabilidade)
+            console.log('Durabilidade ' + durabilidade.nomeProduto + ' atualizada')
+        })
+    })
+
+}
+
+function atualizarDurabilidade(durabilidade) {
+    database.collection(collectionDurabilidade).update(
+        { 'nomeProduto': durabilidade.nomeProduto }, // procurarei pela durabilidade específica
+        {
+            "$set": {
+                "total": durabilidade.total,
+                "unidade": durabilidade.unidade
+            }
+        },
+        { 'multi': false, 'upsert': true }, // como quero atualizar apenas uma durabilidade
+        (err, result, status) => {
+            if (err || result <= 0) {
+                console.log(err)
+                return
+            }
+
+            console.log('Durabilidade ' + durabilidade.nomeProduto + ' alterada com sucesso.')
+        })
+}
 
 /**
  * Rota que persiste no banco de dados um novo produto
